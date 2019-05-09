e2b2908a99e0f893d61fef2e6071cad6464dda52
mc123mp <50111106+mc123mp@users.noreply.github.com>
2019-05-10 00:47:01 +0200

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -1,87 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class CameraController : MonoBehaviour
 {
-    public Vector3 rtsOffset;
-    public float rtsDownwardRotation;
-    Transform lerpTarget;
-    Vector3 targetAngle;
-    bool IsRTS;
+    public Transform spawnPoint; //this is where the rts camera starts at
 
-    Transform currentView; //current view
-    public Transform spawnPoint;
-    RTSController rtsController;
-    FPSController fpsController;
-    
+    RTSController rtsController; //the rtsController script attached to the camera
+
+    Transform lerpTarget; //the transform the camera is supposed to lerp to
+
+    bool isLerping = false;
+
     public float transitionSpeed = 5f;
-    bool IsLerping = false;
 
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 rtsOffset; //distance the camera moves back and up when going back to rts camera   
+
+    private void Start()
     {
         rtsController = this.GetComponent<RTSController>();
-        //fpsController = this.GetComponent<FPSController>(); //implement later
-        transform.position = spawnPoint.transform.position + rtsOffset;
-        targetAngle.x = rtsDownwardRotation;
-        IsRTS = true;
+        this.transform.position = spawnPoint.transform.position;
     }
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (rtsController.GetTargetUnit() && IsRTS)
+            if (rtsController.GetTargetUnit())
             {
                 lerpTarget = rtsController.GetTargetUnit().transform.Find("CameraHook").transform;
-                //print("Target: " + lerpTarget);
-                IsLerping = true;
-                IsRTS = false;
-                rtsController.enabled = false;
+
+                isLerping = true;
             }
         }
-
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            if (!IsRTS)
-            {
-                rtsController.enabled = true;
-                lerpTarget.position = transform.position + rtsOffset;
-                lerpTarget.transform.localEulerAngles = targetAngle;
-                //print("Target pos: " + lerpTarget.position + ", target angle x: " + lerpTarget.transform.localEulerAngles.x);
-                IsLerping = true;
-                IsRTS = true;
-            }
+            lerpTarget.position = this.transform.position + rtsOffset;
+            isLerping = true;
         }
     }
 
     private void FixedUpdate()
     {
-        if (IsLerping)
+        if (isLerping)
             Lerp(lerpTarget);
-    }    
+    }
 
     void Lerp(Transform targetPosition)
     {
-        transform.position = Vector3.Lerp(transform.position, targetPosition.transform.position, Time.deltaTime * transitionSpeed);
-
-        Vector3 currentAngle = new Vector3(
-            Mathf.LerpAngle(transform.rotation.eulerAngles.x, targetPosition.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
-            Mathf.LerpAngle(transform.rotation.eulerAngles.y, targetPosition.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
-            Mathf.LerpAngle(transform.rotation.eulerAngles.z, targetPosition.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
-
-        transform.eulerAngles = currentAngle;
+        this.transform.position = Vector3.Lerp(transform.position, targetPosition.transform.position, Time.deltaTime * transitionSpeed);       
 
         if (Vector3.SqrMagnitude(transform.position - lerpTarget.position) < 0.001)
         {
-            IsLerping = false;
-            print("Final pos: " + lerpTarget.position);
+            isLerping = false;
         }
     }
 
     public bool GetIsLerping()
     {
-        return IsLerping;
+        return isLerping;
     }
 }
